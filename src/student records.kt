62aedebe5adeb04a5d5ee3283f8697eab2d90a52
09winnie <winfredgraceekendi@gmail.com>
2021-02-20 22:40:09 +0300

    first commit

---
+++ src/student records.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val name:String="melannieTamara"
+    var age=23
+    val phoneNumber="0750546787"
+    var weight="57"+ "kgs"
+    val nationality:String ="kenyan"
+    val  kenyan= false
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(kenyan)
+}
